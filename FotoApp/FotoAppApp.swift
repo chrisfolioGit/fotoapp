43ecbe1e673d6cb6b9fa784b25526340b2fea236
herrchristoph <christoph.wojciechowski@alphaneun.de>
2023-02-28 19:34:45 +0100

    Initial Commit

---
+++ FotoApp/FotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoAppApp.swift
+//  FotoApp
+//
+//  Created by Christoph Wojciechowski on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
